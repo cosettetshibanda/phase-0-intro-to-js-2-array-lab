3d933bd031d800dcf6aecc0979e9be5da2e1854c
Cosette Tshibanda <cosettetshibanda@gmail.com>
2022-12-01 22:28:46 -0700

    commit tree 036759660961e83877eac2e07f681eb885a239c6

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = [
+    "Milo", "Otis", "Garfield"
+]
+function destructivelyAppendCat(Ralph) {
+    cats.push("Ralph")
+    console.log(cats)
+}
+function destructivelyPrependCat (Bob){
+    cats.unshift ("Bob")
+    console.log(cats)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    console.log(cats)
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    console.log(cats)
+}
+function appendCat(name) {
+    return [...cats, name]
+}
+function prependCat(name) {
+    return [name, ...cats,]
+}
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1)
+}
+function removeFirstCat() {
+    return cats.slice(-2)
+}
+function removeFirstCat() {
+    return cats.slice(1)
+}
